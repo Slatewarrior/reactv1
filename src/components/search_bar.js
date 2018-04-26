6986c701995b70a73987930b6f5aa28044712e3e
Slatewarrior <ast_000@hotmail.com>
2018-04-26 15:52:32 -0500

    upload 3:52

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -4,15 +4,14 @@ class SearchBar extends Component {
   constructor(props){
     super(props);
 
-    this.state = { term : 'dddd' }
+    this.state = { term : '' }
   }
   render() {
     return (
       <div>
         <input onChange={  event => this.setState({ term : event.target.value}) } 
         value = {this.state.term}
-        />
-        Value of input {this.state.term}
+        /> 
       </div>
     );
   } 
--- src/index.js
+++ src/index.js
@@ -1,13 +1,34 @@
-import React from "react";
+import React, { Component } from "react";
 import ReactDOM from "react-dom";
-import SearchBar from './components/search_bar';
+import SearchBar from "./components/search_bar";
+import YTSearch from "youtube-api-search";
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
-const API_KEY = 'AIzaSyBk2uu1qE6ZAdX8l1EkRZwgwtwImDo81i8';
+const API_KEY = "AIzaSyBk2uu1qE6ZAdX8l1EkRZwgwtwImDo81i8";
 
-const App = ()  => {
-  return <div>
-    <SearchBar />
-  </div>;
-};
-ReactDOM.render(<App />, document.querySelector('.eliseo'));
-//ReactDOM.render(<App />, document.querySelector('.eliseo'));
+
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = { videos: []};
+
+    YTSearch({ key: API_KEY, term: "reactjs tutorial" },  videos => {
+      this.setState( { videos });
+      console.log(videos);
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <VideoDetail video={this.state.videos[0]} />
+        <VideoList videos={this.state.videos} />
+      </div>
+    );
+  }
+} 
+
+ReactDOM.render(<App />, document.querySelector(".eliseo"));
+//export default App;
